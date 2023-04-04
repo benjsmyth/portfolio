1f97fca83cb6e21ca043c4bc41f06f542ba5c965
Ben <38275848+benjsmyth@users.noreply.github.com>
2023-04-04 09:14:47 -0400

    Fix mistake

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -8,7 +8,7 @@
     <footer class="flex justify-between p-3">
       <div class="flex gap-1 justify-between">
         <a class="transition ease-in-out hover:text-amber-900" href="https://github.com/benjsmyth" target="_blank">GitHub</a>
-          <ExternalLinkIcon class="place-self-center h-5 w-5"/>
+        <ExternalLinkIcon class="place-self-center h-5 w-5"/>
       </div>
       <span class="font-thin">Built by me.</span>
     </footer>

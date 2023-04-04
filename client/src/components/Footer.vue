ca5e57b70395eecbfcbcabd24e2e06988b9bfa4a
Ben <38275848+benjsmyth@users.noreply.github.com>
2023-04-04 09:14:20 -0400

    Improve style formatting

--- client/src/components/Footer.vue
+++ client/src/components/Footer.vue
@@ -1,20 +1,16 @@
 <script lang="ts">
-    import { ExternalLinkIcon } from '@heroicons/vue/solid'
-
-    export default {
-        components: { ExternalLinkIcon }
-    }
+  import { ExternalLinkIcon } from '@heroicons/vue/solid'
+  export default { components: { ExternalLinkIcon } }
 </script>
 
 <template>
-    <!-- Footer -->
-    <div id="footer-wrapper">
-        <footer class="flex justify-between p-3">
-            <div class="flex gap-1 justify-between">
-                <a class="transition ease-in-out hover:text-amber-900" href="https://github.com/benjsmyth" target="_blank">GitHub</a>
-                <ExternalLinkIcon class="place-self-center h-5 w-5"/>
-            </div>
-            <span class="font-thin">Built by me.</span>
-        </footer>
-    </div>
+  <div id="footer-wrapper">
+    <footer class="flex justify-between p-3">
+      <div class="flex gap-1 justify-between">
+        <a class="transition ease-in-out hover:text-amber-900" href="https://github.com/benjsmyth" target="_blank">GitHub</a>
+          <ExternalLinkIcon class="place-self-center h-5 w-5"/>
+      </div>
+      <span class="font-thin">Built by me.</span>
+    </footer>
+  </div>
 </template>

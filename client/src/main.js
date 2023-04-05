202f6bddddbebb03c83daf412be2a1a86977179d
Ben <38275848+benjsmyth@users.noreply.github.com>
2023-04-05 06:33:48 -0400

    Add semicolons

--- client/src/main.js
+++ client/src/main.js
@@ -10,20 +10,20 @@ const app = createApp(App).use(router);
 
 // Provide briefs
 axios.get('http://localhost:8000/briefs/').then(response => {
-    app.provide('briefs', response.data)
+    app.provide('briefs', response.data);
 })
 .catch(error => {console.log("error fetching briefs\n", error)});
 
 // Provide projects
 axios.get('http://localhost:8000/projects/').then(response => {
-    app.provide('projects', response.data)
+    app.provide('projects', response.data);
 })
 .catch(error => {console.log("error fetching projects\n", error)});
 
 // Provide tags
 axios.get('http://localhost:8000/tags/').then(response => {
     console.log(response.data)
-    app.provide('tags', response.data)
+    app.provide('tags', response.data);
 })
 .catch(error => {console.log("error fetching tags\n", error)});
 

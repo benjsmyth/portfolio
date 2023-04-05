47f9460a8660d4da663fa1c543d8c04529ef0bcc
Ben <38275848+benjsmyth@users.noreply.github.com>
2023-04-05 06:32:38 -0400

    Improve comments and formatting

--- client/src/main.js
+++ client/src/main.js
@@ -1,27 +1,31 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import axios from 'axios'
-import router from './router'
+import { createApp } from 'vue';
+import axios from 'axios';
+
+// Application imports
+import App from './App.vue';
+import router from './router';
 
 // Create application
-const app = createApp(App).use(router)
+const app = createApp(App).use(router);
 
-// Provide JSON from Django ORM
+// Provide briefs
 axios.get('http://localhost:8000/briefs/').then(response => {
     app.provide('briefs', response.data)
 })
-.catch(error => {console.log("error fetching briefs\n", error)})
+.catch(error => {console.log("error fetching briefs\n", error)});
 
+// Provide projects
 axios.get('http://localhost:8000/projects/').then(response => {
     app.provide('projects', response.data)
 })
-.catch(error => {console.log("error fetching projects\n", error)})
+.catch(error => {console.log("error fetching projects\n", error)});
 
+// Provide tags
 axios.get('http://localhost:8000/tags/').then(response => {
     console.log(response.data)
     app.provide('tags', response.data)
 })
-.catch(error => {console.log("error fetching tags\n", error)})
+.catch(error => {console.log("error fetching tags\n", error)});
 
 // Mount application
-app.mount('#app')
+app.mount('#app');

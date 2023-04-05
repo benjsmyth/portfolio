92cc629a509fad8c24753d936f9677ad8d55ef9d
Ben <38275848+benjsmyth@users.noreply.github.com>
2023-04-05 00:01:19 -0400

    Improve formatting

--- client/src/views/Tags.vue
+++ client/src/views/Tags.vue
@@ -3,17 +3,14 @@
 </script>
 
 <script lang="ts">
-  export default {
-    inject: ['tags']
-  }
+  export default { inject: ['tags'] }
 </script>
 
 <template>
-    <!-- Tags wrapper -->
-    <div id="tags-wrapper">
-      <Tag class="m-4 text-right" v-for="tag in tags"
-          :key="tag.id"
-          :keyword="tag.keyword"
-      />
-    </div>
+  <div id="tags-wrapper">
+    <Tag class="m-4 text-right" v-for="tag in tags"
+      :key="tag.id"
+      :keyword="tag.keyword"
+    />
+  </div>
 </template>
